0c1be88642eb79c037a8727f20b5d3aa633ea4a8
Reinoldo <reinoldo@outlook.com>
2021-02-12 00:03:03 +0000

    aws provider config.

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: 'EU (Ireland) eu-west-1',
+        region: env('AWS_REGION'),
         params: {
-          Bucket: 'reactavancado-studies',
+          Bucket: env('AWS_BUCKET'),
         },
       },
     },
